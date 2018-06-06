b6e8a2a73a5b5a691f9ebe8081db8690fe291233
Çağhan <caghandemir@gmail.com>
2018-06-06 21:50:56 +0300

    Revert "Add requisite code to cpufreq.h to fix Darkness Governor"
    This reverts commit 91db58ce3167c4f968e14fe1172888c87e4d364d.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -531,9 +531,6 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
-extern struct cpufreq_governor cpufreq_gov_darkness;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
